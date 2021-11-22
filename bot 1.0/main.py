2857e5ab318225562d45e3fbd35d436cf12fa617
Harsh-2200 <harshharodenice1@gmail.com>
2021-11-22 17:46:09 +0530

    1.0

---
+++ bot 1.0/main.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+from discord.flags import Intents
+import music
+
+cogs = [music]
+
+client = commands.Bot(command_prefix="?" , Intents = discord.Intents.all())
+
+
+for i in range(len(cogs)):
+    cogs[i].setup(client)
+
+
+client.run("OTExODQ5MDA0NTU4NjEwNDcy.YZnXkA.ttBH0iwA2YyY__JJOKchQ6CRxXQ")
+
+ 
--- bot 1.0/music.py
+++ bot 1.0/music.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.player import FFmpegPCMAudio
 import youtube_dl
 
 class music(commands.Cog):
@@ -9,7 +10,7 @@ class music(commands.Cog):
     @commands.command()
     async def join(self , ctx):
         if ctx.author.voice is None:
-            await ctx.send("bsdk voice channel tho join kr lo ")
+            await ctx.send("pls join the voice channel first")
         voice_channel = ctx.auth.voice.channel
         if ctx.voice_client is None:
             await voice_channel.connect()
@@ -24,7 +25,24 @@ class music(commands.Cog):
     async def play(self , ctx , url ):
         if ctx.voice_client.stop():
             FFMPEG_OPTIONS = {'before_options' : '-reconnect 1 -reconnect_streamed 1 -reconnect_deelay' }
+            YDL_OPTIONS = {'format' : 'bestaudio' }
+            vc = ctx.voice_client 
 
+            with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl :
+                info = ydl.extract_info(url , download=False  )
+                url2 = info['formats'][0]['url']
+                source = await discord.FFmpegOpusAudio.from_probe(url2,**FFMPEG_OPTIONS)
+                vc.play(source)
+
+    @commands.command()
+    async def pause(self , ctx):
+        await ctx.voice_client.pause()
+        await ctx.send("Paused"
+        
+    )@commands.command()
+    async def resume(self , ctx):
+        await ctx.voice_client.resume()
+        await ctx.send("Resume")
 
 
 
