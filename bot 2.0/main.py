4d10e8c1f9dd8421025b4cfde417734af01c7cd1
Harsh-2200 <harshharodenice1@gmail.com>
2021-11-25 21:16:21 +0530

    token

--- bot 2.0/main.py
+++ bot 2.0/main.py
@@ -1,4 +1,29 @@
+from discord.ext import commands
 
+bot = commands.Bot(command_prefix="!")
+
+
+bot.lava_nodes = [
+    {
+        'host' : 'lava.link',
+        'port' : 80,
+        'rest_uri' : f'http://lava.link:80',
+        'identifier' : 'MAIN' ,
+        'password' : 'anything', 
+        'region' : 'india'
+    }
+]
+
+@bot.event
+async def on_ready():
+    print("Bot is ready to play music")
+    bot.load_extension('dismusic')
+    
+
+
+
+
+bot.run('')
 
 
 
